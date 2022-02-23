21d8066c0a31c2b507d9a82ac9cb8d691fe53aef
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-02-23 16:08:39 +0300

    add test function to test for correct innitialization

--- app/models/movie_test.py
+++ app/models/movie_test.py
@@ -1,16 +1,22 @@
-from os import unlink
 import unittest
-from models import movie
+import movie
 Movie = movie.Movie
 
 class TestMovie(unittest.TestCase):
 
     def setUp(self):
-        self.new_movie = Movie(1234,'Python Must Be Crazy','A thrilling new Python Series','https://image.tmdb.org/t/p/w500/khsjha27hbs',8.5,129993)
+        self.new_movie = Movie(1234,'Python Must Be Crazy','A thrilling new Python Series','khsjha27hbs',8.5,129993)
 
     def test_instance(self):
         self.assertTrue(isinstance(self.new_movie, Movie)) #isinstance checks if 
 
+    def test__init__(self):
+        self.assertEqual(self.new_movie.id, 1234)
+        self.assertEqual(self.new_movie.title, 'Python Must Be Crazy')
+        self.assertEqual(self.new_movie.overview, 'A thrilling new Python Series')
+        self.assertEqual(self.new_movie.poster, 'https://image.tmdb.org/t/p/w500/' + 'khsjha27hbs')
+        self.assertEqual(self.new_movie.vote_average, 8.5)
+        self.assertEqual(self.new_movie.vote_count, 129993)
 
 if __name__ == '__main__':
     unittest.main()
