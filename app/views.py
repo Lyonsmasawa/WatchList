6bc44f577c799a4a69a7030215d8e0be62d23ab5
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-02-23 13:05:49 +0300

    create index route

---
+++ app/views.py
@@ -0,0 +1,9 @@
+from operator import imod
+from flask import render_template #takes in the name of a template file as an argument and automatically searches for the template file
+#in our app/templates/subdirectory and loads it 
+from app import app #we import app instance from app folder
+
+@app.route('/') #route decorator
+def index():  #view function
+    """ View root page """
+    return render_template('index.html')
