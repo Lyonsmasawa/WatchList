7a63110161c17a98d6cf3e31cfc70cc8d03bf2f8
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-02-23 13:20:01 +0300

    variables and variable blocks

--- app/views.py
+++ app/views.py
@@ -1,3 +1,4 @@
+from email import message
 from operator import imod
 from flask import render_template #takes in the name of a template file as an argument and automatically searches for the template file
 #in our app/templates/subdirectory and loads it 
@@ -5,5 +6,7 @@ from app import app #we import app instance from app folder
 
 @app.route('/') #route decorator
 def index():  #view function
+    
     """ View root page """
+    message = 'Hello World'
     return render_template('index.html')
