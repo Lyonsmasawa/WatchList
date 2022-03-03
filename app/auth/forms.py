5eac7fc581a24f0435928c82c02e1674351774f0
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-03-04 01:23:24 +0300

    add welcome email

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -7,7 +7,7 @@ from wtforms import ValidationError
 class RegistrationForm(FlaskForm):
     email = StringField('Your Email Address', validators = [InputRequired(),Email()])
     username = StringField('Enter your username', validators = [InputRequired()])
-    password = PasswordField('Password', validators = [InputRequired(), EqualTo('password confirm', message = 'Passwords must match')])
+    password = PasswordField('Password', validators = [InputRequired(), EqualTo('password_confirm', message = 'Passwords must match')])
     password_confirm = PasswordField('Confirm Password', validators = [InputRequired()])
     submit = SubmitField('Sign Up')
 
